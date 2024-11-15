811493ae19e651639c7e571838a4523ce34b0b54
DevinHorton45 <hortond26@stxtigers.com>
2024-11-15 13:48:29 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,110 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Shoot(1)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+	
+})
+info.onCountdownEnd(function () {
+	
+})
+function Shoot (num: number) {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 9 9 . . . . . . . 
+        . . . . . . 9 9 9 9 . . . . . . 
+        . . . . . . 9 9 9 9 . . . . . . 
+        . . . . . 9 9 9 9 9 9 . . . . . 
+        . . . . . 9 9 9 9 9 . . . . . . 
+        . . . . . 9 9 9 9 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 0, 0)
+    projectile.follow(myenemy2, 100)
+    info.startCountdown(0.5)
+    pauseUntil(() => info.countdown() == 0)
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    sprites.destroy(sprite)
+    info.changeScoreBy(1)
+})
+let projectile: Sprite = null
+let myenemy2: Sprite = null
+let mySprite2 = 0
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+info.setScore(0)
+controller.moveSprite(mySprite, 27, 27)
+for (let index = 0; index < 2000; index++) {
+    mySprite2 = randint(1, 2)
+    if (mySprite2 == 1) {
+        myenemy2 = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . 7 7 . . . . 7 7 . . . . 
+            . . . . 7 7 7 7 7 7 7 7 . . . . 
+            . . . . 6 6 7 7 7 7 6 6 . . . . 
+            . . . . 6 6 7 7 7 7 6 6 . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        myenemy2.setPosition(0, randint(0, 120))
+    } else {
+        myenemy2 = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . 7 7 . . . . 7 7 . . . . 
+            . . . . 7 7 7 7 7 7 7 7 . . . . 
+            . . . . 6 6 7 7 7 7 6 6 . . . . 
+            . . . . 6 6 7 7 7 7 6 6 . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        myenemy2.setPosition(160, randint(0, 120))
+    }
+    myenemy2.follow(mySprite, 30)
+    pause(200)
+}
+game.onUpdateInterval(500, function () {
+	
+})
